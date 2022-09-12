eee07d86b3f55b4a3be20669a8fac85ed17af7c9
saturnhati <76071008+saturnhati@users.noreply.github.com>
2022-09-12 23:16:57 +0200

    adds decoration and alert and a lot of things

--- js/index.js
+++ js/index.js
@@ -4,6 +4,18 @@ let tired = document.getElementById("tired-frog")
 display.innerHTML = `The frog jumped ${counter} times.`
 let frogImg = document.getElementById("frog")
 let frogIsDead = false
+const TIREDNESSLEVELARR = [20, 30, 40, 50, 60, 70, 80, 90]
+let tiredness;
+let body = document.querySelector('body')
+let yes = document.getElementById("yes")
+let no = document.getElementById("no")
+let modal = document.getElementById("modal")
+
+function randomizer(){
+    tiredness = TIREDNESSLEVELARR[Math.floor(Math.random()*8)]
+}
+randomizer()
+console.log(tiredness)
 
 function jump(){
         document.getElementById('btn').disabled = true
@@ -12,7 +24,10 @@ function jump(){
         setTimeout("removeClass()", 1500)
         hopscount()
         schiatting()
-}
+        if(frogIsDead == true){
+                 modal.classList.remove('invisible')
+            }
+        }
 
 function shrink(){
     document.getElementById("btn").classList.add("shrink")
@@ -31,18 +46,19 @@ function removeClass(){
 function hopscount (){
     counter ++
     display.innerHTML = `The frog jumped ${counter} times.`
-    if (counter == 2){
+    if (counter == 1){
         tired.classList.remove('im-so-sad-im-so-very-very-sad')
         frogImg.src="./images/frog-cry.png"
     }
 }
 
 function schiatting (){
-    if (counter == 3){
+    if (counter == 2){
         frogIsDead = true
         tired.innerHTML = `The frog is dead.`
         tired.style.color = 'red'
         tired.style.fontWeight = 700
+
         frogImg.src="./images/frog-dead.png"
         document.getElementById('btn').disabled = true
     }
@@ -50,3 +66,13 @@ function schiatting (){
         console.log('hops')
     }
 }
+
+yes.addEventListener('click', function(){
+    document.querySelector('h2').innerHTML = "I'm sorry for u..."
+    document.getElementById('button-area').classList.add('invisible')
+})
+
+no.addEventListener('click', function(){
+    body.classList.add('red') 
+    modal.classList.add('invisible')
+})
